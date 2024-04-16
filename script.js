e24deabd227fdde0378840cbde06d34bc167d5a1
Enzo-Martolini <enzo.martolini@laplateforme.io>
2024-04-16 13:31:12 +0200

    association du front avec les fonctions

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let button = document.getElementById("click");
+let button = document.getElementById("sectionClicker");
 let button50 = document.getElementById("bonus50");
 let button125 = document.getElementById("bonus125");
 let button500 = document.getElementById("bonus500");
 let button1000 = document.getElementById("bonus1000");
 
-let numberOfClick = 45;
+let div1 = document.getElementById("1")
+let div2 = document.getElementById("2")
+let div3 = document.getElementById("3")
+let div4 = document.getElementById("4")
+let div5 = document.getElementById("5")
+let div6 = document.getElementById("6")
+let div7 = document.getElementById("7")
+let div8 = document.getElementById("8")
+let div9 = document.getElementById("9")
+let div10 = document.getElementById("10")
+
+
+let numberOfClick = 500;
+
 if (localStorage.getItem("nombreDeClick")){
     numberOfClick = localStorage.getItem("nombreDeClick");
 }
@@ -46,11 +59,15 @@ button50.addEventListener('click', startAutoClicker);
 button.addEventListener('click', function () {
     numberOfClick++;
     console.log(numberOfClick)
+    updateCompteur();
+
     localStorage.setItem('nombreDeClick', numberOfClick);
 });
 
 function startAutoClicker () {
-    if (numberOfClick >= 50 || bonus50 ==true) {
+    if (numberOfClick >= 50 && bonus50 !=true) {
+        interval=1000;
+        numberOfClick-=50;
         autoClicker();
         startInterval();
         bonus50=true;
@@ -65,6 +82,48 @@ function autoClicker() {
         localStorage.setItem('nombreDeClick', numberOfClick);
 
         console.log(numberOfClick)
+        updateCompteur();
+
+        if (numberOfClick >= 100) {
+            div1.classList.remove("false");
+        }
+        
+        if (numberOfClick >= 200) {
+            div2.classList.remove("false");
+        }
+        
+        if (numberOfClick >= 300) {
+            div3.classList.remove("false");
+        }
+        
+        if (numberOfClick >= 400) {
+            div4.classList.remove("false");
+        }
+
+        if (numberOfClick >= 500) {
+            div5.classList.remove("false");
+        }
+
+        if (numberOfClick >= 600) {
+            div6.classList.remove("false");
+        }
+
+        if (numberOfClick >= 700) {
+            div7.classList.remove("false");
+        }
+
+        if (numberOfClick >= 800) {
+            div8.classList.remove("false");
+        }
+
+        if (numberOfClick >= 900) {
+            div9.classList.remove("false");
+        }
+        
+        if (numberOfClick >= 1000) {
+            div10.classList.remove("false");
+        }
+        
     }
 
 function startInterval () {
@@ -73,32 +132,36 @@ function startInterval () {
 
 
 button125.addEventListener('click', function () {
-    if (numberOfClick >= 100 || bonus125 ==true) {
+    if (numberOfClick >= 125 && bonus125 !=true) {
     bonus125 = true;
     localStorage.setItem('bonus125', bonus125);
     interval = 500;
+    numberOfClick -= 125
     updateInterval();
+    console.log(interval)
     console.log(bonus125)
     }
 });
 
 button500.addEventListener('click', function () {
-    if (numberOfClick >= 500 || bonus500 ==true) {
+    if (numberOfClick >= 500 && bonus500 !=true) {
     bonus500 = true;
     localStorage.setItem('bonus500', bonus500);
     interval = 250;
     updateInterval();
     console.log(bonus500)
+    numberOfClick-=500;
     }
 });
 
 button1000.addEventListener('click', function () {
-    if (numberOfClick >= 1000 || bonus1000 ==true) {
+    if (numberOfClick >= 1000 && bonus1000 !=true) {
     bonus1000 = true;
     localStorage.setItem('bonus1000', bonus1000);
     interval = 125;
     updateInterval();
     console.log(bonus1000)
+    numberOfClick-=1000;
     }
 });
 
@@ -124,7 +187,10 @@ function updateInterval() {
     intervalID = setInterval(autoClicker, interval); // Définit le nouvel intervalle
 }
 
-
+function updateCompteur () {
+   let compteur = document.getElementById("compteur");
+   compteur.textContent = numberOfClick;
+}
 
 
 
